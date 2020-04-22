2ccb28b2272f61f2554f68495b7cfe2698b5139d
Scott Smith <scthsm@outlook.com>
2020-04-22 17:17:32 -0600

    a

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,11 +6,54 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement !== 20){
+  return { ...item,
+  enhancement: item.enhancement + 1
+  };
+   } else {
+    return { ...item };
+   }
 }
 
 function fail(item) {
-  return { ...item };
+//   if (item.enhancement < 15 && item.durability > 4){
+//   return { ...item,
+//   durability: item.durability - 5
+//   }
+//    } else 
+//    if (item.enhancement == 15 && item.durability > 9){
+//     return { ...item,
+//     durability: item.durability - 10
+//     }
+// } else if (item.enhancement >= 16 && item.durability > 9){
+//  return { ...item,
+//   enhancement: item.enhancement -1,
+//   durability: item.durability - 10
+//  }
+// } else if (item.enhancement >= 16 && item.durability < 9){
+//   return { ...item,
+//    enhancement: item.enhancement -1,
+//    durability: 0
+//   }
+//  } else if (item.enhancement == 15 && item.durability < 9){
+//   return { ...item,
+//    durability: 0
+//   }
+//  } else if (item.enhancement < 15 && item.durability < 5){
+//   return { ...item,
+//    durability: 0
+//   }
+//  }
+if (item.enhancement < 15) item.durability -= 5;
+
+if (item.enhancement >= 15) item.durability -= 10;
+
+if (item.enhancement > 16) item.enhancement -= 1;
+
+if (item.durability < 0) item.durability = 0;
+
+return { ...item };
+
 }
 
 function repair(item) {
@@ -20,5 +63,6 @@ function repair(item) {
 }
 
 function get(item) {
+  if (item.enhancement > 0) item.name = `[+${item.enhancement}] ${item.name}`;
   return { ...item };
 }
